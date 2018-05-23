4b1b52206edacdeefa2c721cfb051ed7396d552d
Cristian Jaramillo <cristian_gerar@hotmail.com>
2018-05-23 09:44:14 -0500

    Initial application

---
+++ app/src/main/java/mx/jccg/running/MainActivity.java
@@ -0,0 +1,20 @@
+package mx.jccg.running;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    /**
+     *
+     * @param savedInstanceState
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
