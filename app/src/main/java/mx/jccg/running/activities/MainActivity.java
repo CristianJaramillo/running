4aaa62639c6e82ee9c37fd652fbb7bcfa528f87d
Cristian Jaramillo <cristian_gerar@hotmail.com>
2018-05-27 04:49:54 -0500

    Start counter of application

--- app/src/main/java/mx/jccg/running/activities/MainActivity.java
+++ app/src/main/java/mx/jccg/running/activities/MainActivity.java
@@ -1,22 +1,106 @@
 package mx.jccg.running.activities;
 
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.os.Build;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.TextView;
 
 import mx.jccg.running.R;
+import mx.jccg.running.time.StartCountDownTimer;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener
+{
+
+    /**
+     *
+     */
+    private TextView timeTextView;
+
+    /**
+     *
+     */
+    private FloatingActionButton startFloatingActionButton;
+
+    /**
+     *
+     */
+    private StartCountDownTimer startCountDownTimer;
+
+    /**
+     *
+     */
+    public static Boolean START = null;
 
     /**
      *
      * @param savedInstanceState
      */
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        timeTextView = findViewById(R.id.time_text_view);
+
+        startFloatingActionButton = findViewById(R.id.start_floating_action_button);
+        startFloatingActionButton.setOnClickListener(this);
+
+        startCountDownTimer = new StartCountDownTimer(timeTextView);
     }
+
+    /**
+     *
+     * @param view
+     */
+    @Override
+    public void onClick(View view)
+    {
+        if(START == null)
+        {
+            START = true;
+
+            startCountDownTimer.start();
+
+            startFloatingActionButton.setBackgroundTintList(ColorStateList
+                    .valueOf(Color.parseColor("#EC3951")));
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                startFloatingActionButton.setImageDrawable(getDrawable(R.drawable.ic_stop));
+            }
+
+        } else if(START) {
+
+            START = false;
+
+            startCountDownTimer.start();
+
+            startFloatingActionButton.setBackgroundTintList(ColorStateList
+                    .valueOf(Color.parseColor("#8BE559")));
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                startFloatingActionButton.setImageDrawable(getDrawable(R.drawable.ic_play_arrow));
+            }
+
+        } else {
+
+            START = true;
+
+            startFloatingActionButton.setBackgroundTintList(ColorStateList
+                    .valueOf(Color.parseColor("#EC3951")));
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                startFloatingActionButton.setImageDrawable(getDrawable(R.drawable.ic_stop));
+            }
+
+        }
+
+    }
+
 }
---
+++ app/src/main/java/mx/jccg/running/time/StartCountDownTimer.java
@@ -0,0 +1,65 @@
+package mx.jccg.running.time;
+
+import android.os.CountDownTimer;
+import android.util.Log;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+
+import mx.jccg.running.activities.MainActivity;
+
+/**
+ *
+ * @author Cristian Jaramillo (cristian_gerar@hotmail.com)
+ */
+public class StartCountDownTimer extends CountDownTimer
+{
+    static final String TAG = StartCountDownTimer.class.getName();
+
+    static final long COUNT_DOWN_INTERVAL = 1000;
+    static final long MILLIS_IN_FUTURE =  86400000;
+
+    /**
+     *
+     */
+    private TextView textView;
+
+    /**
+     *
+     * @param textView
+     */
+    public StartCountDownTimer(TextView textView)
+    {
+        super(MILLIS_IN_FUTURE, COUNT_DOWN_INTERVAL);
+
+        this.textView = textView;
+    }
+
+    /**
+     *
+     * @param l
+     */
+    @Override
+    public void onTick(long l)
+    {
+        if(!MainActivity.START)
+            onFinish();
+        else {
+            long second = ((MILLIS_IN_FUTURE - l) / COUNT_DOWN_INTERVAL) % 60;
+            long minute = ((MILLIS_IN_FUTURE - l) / (COUNT_DOWN_INTERVAL * 60)) % 60;
+            long hour   = ((MILLIS_IN_FUTURE - l) / (COUNT_DOWN_INTERVAL * 60 * 60)) % 24;
+
+            textView.setText(String.format("%02d:%02d:%02d", hour, minute, second));
+        }
+    }
+
+    /**
+     *
+     */
+    @Override
+    public void onFinish() {
+        Log.i(TAG, "Finish");
+        textView.setText("00:00:00");
+    }
+
+}
